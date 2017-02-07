0ce4cd68e7093da407ab090afd72331795877930
Shashank Srivastava <shashank.ssriv@gmail.com>
2017-02-07 09:04:59 +0100

    Add files via upload
    SQL Script to create database tables.

---
+++ gentables.sql
@@ -0,0 +1,2 @@
+CREATE TABLE employee (id VARCHAR(7), name VARCHAR(50), acc VARCHAR(25), manager_name VARCHAR(50), exp_nature varchar(30), pmtdt varchar(20));
+CREATE TABLE collectdata (id VARCHAR(7), submission_date VARCHAR(10), date VARCHAR(10), brdtm VARCHAR(7), amount double, rate double);
